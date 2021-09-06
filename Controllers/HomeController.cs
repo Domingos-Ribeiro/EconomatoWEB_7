b480fe1f66d2aace883a37417c0c34beed975691
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-09-06 02:32:38 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EconomatoWEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            ViewBag.Message = "Economato WEB";
+
+            return View();
+        }
+
+        public ActionResult Contactos()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+    }
+}
