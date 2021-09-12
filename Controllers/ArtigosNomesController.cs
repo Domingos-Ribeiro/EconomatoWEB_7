158acd21b4db9d51868fc1d6d5097fb9bf4a3f77
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-09-12 17:28:23 +0100

    edit

---
+++ Controllers/ArtigosNomesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EconomatoWEB.DAL;
+
+namespace EconomatoWEB.Controllers
+{
+    public class ArtigosNomesController : Controller
+    {
+
+        EconomatoWEBContext pointer = new EconomatoWEBContext();
+
+        // GET: ArtigosNomes
+        public ActionResult Index()
+        {
+            //Transporte pelo modelo ou Mdel usando o LINQ
+            //A Variavel "TodosOsArtigos" é que vai ficar com o nome de todos os artigos
+            var TodosOsArtigos = pointer.Artigos.ToList();
+
+            return View(TodosOsArtigos);
+        }
+    }
+}
---
+++ Controllers/FiltroNomeArtigosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EconomatoWEB.DAL;
+
+namespace EconomatoWEB.Controllers
+{
+    public class FiltroNomeArtigosController : Controller
+    {
+        EconomatoWEBContext db = new EconomatoWEBContext();
+
+        // GET: FiltroNomeArtigos
+        public ActionResult Index(string Filtro)
+        {
+            var NomeArtigos = db.Artigos.ToList();
+
+            if (!string.IsNullOrEmpty(Filtro))
+            {
+                NomeArtigos = db.Artigos.Where(g => g.Nome.Contains(Filtro)).ToList();
+            }
+
+            return View(NomeArtigos);
+        }
+    }
+}
